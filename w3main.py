cd752fd93c3b5aacccb8437597019639ecfd66b0
gitrepo <sejinpark318@naver.com>
2016-03-25 07:27:12 +0900

    f_c_transfer
    f_c_transfer code

---
+++ w3main.py
@@ -0,0 +1,19 @@
+﻿def transfer_c_f():
+    cf=raw_input("celsius of fahreheit: ")
+
+    if(cf=='c'):
+        print "You entered celsius, please input celsisus value"
+        temp=raw_input("celsius value: ")
+        f_temp=float(temp)
+        tem=(f_temp *1.8)+32
+        print tem
+    else:
+        print "You entered fahreheit, please input fahreheit value"
+        temp=raw_input("fahreheit value:")
+        f_temp=float(temp)
+        tem=(f_temp-32)/1.8
+    
+    print tem
+    
+    
+transfer_c_f()
