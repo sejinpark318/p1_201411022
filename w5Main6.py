404756275348ea4da27b4574b9b138d21d1fa75d
박세진 p1_201411022 <sejinpark318@naver.com>
2016-03-30 15:25:43 +0900

    computeGrade

---
+++ w5Main6.py
@@ -0,0 +1,18 @@
+﻿inmarks=raw_input("input your score(0~100): ")
+marks=float(inmarks)
+def computeGrade(marks):
+
+    if (marks>=90 and marks<=100):
+        grade='A'
+    elif (marks>=80 and marks<90):
+        grade='B'
+    elif (marks>=70 and marks<80):
+        grade='C'
+    elif (marks>=60 and markse<70):
+        grade='D'
+    else:
+        grade='F'
+    
+    print "Your grade is " +grade
+    
+computeGrade(marks)
