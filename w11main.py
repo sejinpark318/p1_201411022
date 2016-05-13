91ec5fa872cd300c79bc7fe558c58ccacd42e63e
박세진 p1_201411022 <sejinpark318@naver.com>
2016-05-13 09:16:07 +0900

    Add files via upload

---
+++ w11main.py
@@ -0,0 +1,36 @@
+﻿
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+def keyup():
+	t1.fd(50)
+def keydown():
+	t1.back(50)
+def keyright():
+	t1.right(90)
+def keyleft():
+	t1.left(90)
+
+
+
+wn.onkey(keyup, "Up")
+wn.onkey(keydown, "Down")
+wn.onkey(keyright, "Right")
+wn.onkey(keyleft, "Left")
+
+wn.listen()
+
+turtle.mainloop()
+
+def lab11():
+
+
+
+def main():
+	lab11()
+
+
+if__name__=="__main__":
+	main()
